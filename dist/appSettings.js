e193df67bee3e3c6b8b2e3cd643d6170c7892332
Vladimir Zotov <azuresplash@gmail.com>
2017-06-23 12:41:57 +0300

    Removed dist folder

--- dist/appSettings.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appSettings = {
-    chatServerPort: 8000
-};
-//# sourceMappingURL=appSettings.js.map
--- dist/createAvatarUrl.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function createAvatarUrl(key) {
-    return "https://robohash.org/" + key + "??size=128";
-}
-exports.createAvatarUrl = createAvatarUrl;
-//# sourceMappingURL=createAvatarUrl.js.map
